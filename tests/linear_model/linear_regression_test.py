593978828acc9814f1d58f3b61d8c8697938524e
lhlin <lianhuilin@126.com>
2022-12-18 00:39:34 +0800

    test: Add linear regression test

--- tests/linear_model/linear_regression_test.py
+++ tests/linear_model/linear_regression_test.py
@@ -2,10 +2,14 @@ from mini_ml.linear_model import LinearRegression
 
 def simple_regression_test():
     X = [[1], [2]]
-    y = [1, 2]
+    y = [2, 4]
 
     reg = LinearRegression()
     reg.fit(X, y)
+
+    X_test = [[3]]
+    y_pred = reg.predict(X_test)
+    print(y_pred)
 
 if __name__ == "__main__":
     simple_regression_test()
