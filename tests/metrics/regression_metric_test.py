0fe27e236babc732de9a3535478843351415b0a3
lhlin <lianhuilin@126.com>
2022-12-18 00:39:45 +0800

    test: Add metric test

--- tests/metrics/regression_metric_test.py
+++ tests/metrics/regression_metric_test.py
@@ -5,7 +5,7 @@ def sum_square_error_test():
     y_true = np.array([3, 2, 5, 9])
     y_predict = np.array([2, 2, 2, 2])
 
-    assert sum_square_error(y_true, y_predict) == 49
+    assert sum_square_error(y_true, y_predict) == 59
 
 if __name__ == "__main__":
     sum_square_error_test()
